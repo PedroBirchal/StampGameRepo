865e95c2045503e91135d18aa23db3e5ee79e9a7
Pedro de Lima Birchal (Lima) <birchalpedro@gmail.com>
2026-02-19 12:45:06 -0300

    eu estou comitando crimes!

--- Scripts/CartaController.gd
+++ Scripts/CartaController.gd
@@ -1,9 +1,16 @@
 extends Node3D
 
 @onready var rb : RigidBody3D = $Carta
+@onready var cena_carta = preload("res://Scenes/Carta.tscn")
 @export var force: float
 
 func on_target_clicked(target_position) -> void:
 	var direction = target_position - rb.global_position  
 	print(direction)
 	rb.apply_impulse(direction.normalized() * force)
+	instantiate_new_carta()
+
+func instantiate_new_carta() -> void:
+	var nova_carta = cena_carta.instantiate()
+	add_child(nova_carta)
+	rb = nova_carta
---
+++ Scripts/Singleton.gd
@@ -0,0 +1,6 @@
+extends Node
+
+class_name Destino
+
+@export var nome : StringName
+@export var cor : Color
