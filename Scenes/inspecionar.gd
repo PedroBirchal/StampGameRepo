39f8418b2867c26fe80594768bad6cb0760cb1e9
anaclaragl <ag.linhares2@gmail.com>
2026-02-20 19:40:38 -0300

    Resolvendo inspecionar

--- Scenes/inspecionar.gd
+++ Scenes/inspecionar.gd
@@ -2,7 +2,6 @@ extends RigidBody3D
 
 var rotating : bool = false
 var selecionado : bool = false
-@export var sensibilidade : float = 0.01
 
 func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
@@ -11,19 +10,21 @@ func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, n
 				selecionado = true
 				get_viewport().set_input_as_handled()
 
+var dir : Vector2
+func _physics_process(delta: float):
+	rotate_object_local(Vector3.UP, dir.x * .7 * delta)
+	rotate_object_local(Vector3.RIGHT, dir.y * .7 * delta)
+
 func _input(event):
 	if selecionado:
 		if event is InputEventMouseButton:
-			if event.is_pressed():
-				rotating = true
-			else:
-				rotating = false
+			rotating = event.is_pressed()
 		
 		if event is InputEventMouseMotion and rotating:
-			var rel = event.relative
-			rotate_object_local(Vector3.UP, rel.x * .7 * sensibilidade)
-			rotate_object_local(Vector3.RIGHT, rel.y * .7 * sensibilidade)
-		
+			dir = event.relative
+		else:
+			dir = Vector2.ZERO
+			
 		if event is InputEventMouseButton and event.pressed:
 			if event.button_index == MOUSE_BUTTON_RIGHT:
 				selecionado = false
