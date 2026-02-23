924379b4090ff6f98a37e6b415c46a037c4f540d
Juan Vitor <juanriven10@gmail.com>
2026-02-22 21:07:52 -0300

    🕴️ Quase nada só p não perder se conflitar

--- Scripts/interagivel.gd
+++ Scripts/interagivel.gd
@@ -17,10 +17,12 @@ func interagir() -> void:
 	on_interact.emit()
 
 func _on_hover() -> void:
-	mesh.set_surface_override_material(0, material_outline)
+	if mesh != null:
+		mesh.set_surface_override_material(0, material_outline)
 
 func _on_unhover() -> void:
-	mesh.set_surface_override_material(0, null)
+	if mesh != null:
+		mesh.set_surface_override_material(0, null)
 
 
 func _get_configuration_warnings() -> PackedStringArray:
