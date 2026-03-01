572fea8d770d02f3838f7c406b03e0c147f3f37b
anaclaragl <ag.linhares2@gmail.com>
2026-03-01 19:50:26 -0300

    main menu completo

--- Scripts/Menu/main_menu.gd
+++ Scripts/Menu/main_menu.gd
@@ -2,19 +2,11 @@ extends Node2D
 
 @export var creditsTela : Panel
 @export var opcoesTela : Panel
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+@export var tutorialTela : Panel
+@export var buttonManager : Control
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/quarto/quarto.tscn")
-	pass # Replace with function body.
 
 
 func _on_quit_pressed() -> void:
@@ -22,13 +14,24 @@ func _on_quit_pressed() -> void:
 
 
 func _on_credits_pressed() -> void:
+	buttonManager.visible = false
 	creditsTela.visible = true
 
 
 func _on_texture_button_pressed() -> void:
 	creditsTela.visible = false
 	opcoesTela.visible = false
+	tutorialTela.visible = false
+	buttonManager.visible = true
 
 
 func _on_options_pressed() -> void:
 	opcoesTela.visible = true
+	buttonManager.visible = false
+	
+
+
+func _on_tutorial_pressed() -> void:
+	tutorialTela.visible = true
+	buttonManager.visible = false
+	
