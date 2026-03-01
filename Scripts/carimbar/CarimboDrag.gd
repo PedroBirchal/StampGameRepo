4f065c9adbe3fd8effda8be9753a5543bc51a9fe
anaclaragl <ag.linhares2@gmail.com>
2026-03-01 11:43:47 -0300

    Carimbo brilha quando passa o mouse

--- Scripts/carimbar/CarimboDrag.gd
+++ Scripts/carimbar/CarimboDrag.gd
@@ -154,8 +154,11 @@ func _on_jogo_carimbar_mudou_encomenda(encomenda: Encomenda) -> void:
 
 
 func _on_area_3d_mouse_entered() -> void:
+	if not holding:
+		mesh_carimbo.get_active_material(0).rim_enabled = true
 	carimbo_imagem.visible = true
 
 
 func _on_area_3d_mouse_exited() -> void:
+	mesh_carimbo.get_active_material(0).rim_enabled = false
 	carimbo_imagem.visible = false
