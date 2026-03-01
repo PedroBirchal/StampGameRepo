6f90d1685e99d48a4ec0f6978795772a86962f60
anaclaragl <ag.linhares2@gmail.com>
2026-03-01 12:27:20 -0300

    ☎️ Sorry, I cannot hear you, I'm kinda busy

--- Sounds/AudioManager.gd
+++ Sounds/AudioManager.gd
@@ -3,5 +3,5 @@ extends Node3D
 @onready var land_sfx = $LandSFX
 @onready var open_letter = $OpenLetter
 @onready var close_letter = $CloseLetter
-@onready var telephone = $crank_ringtone
+@onready var telephone = $crank_ringtone2
 @onready var carimbo = $Carimbo
